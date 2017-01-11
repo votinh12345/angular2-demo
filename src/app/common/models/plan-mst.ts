7518c847674dbea8b0052d4ab1528ab223e371ea
NguyenVanHien <nguyenvanhienbk2006@gmail.com>
2017-01-11 17:14:31 +0700

    add new code

---
+++ src/app/common/models/plan-mst.ts
@@ -0,0 +1,26 @@
+export class PlanMst {
+    plan_code: string;
+    plan_name: string;
+    plan_desc: string;
+    plan_class: string;
+    plan_initial_dis: string;
+    plan_start_date: Date;
+    plan_end_date: Date;
+    plan_type: string;
+    plan_last_upd_user: string;
+    plan_last_upd_date: Date;
+
+    constructor(plan_code, plan_name, plan_desc, plan_class, plan_initial_dis,
+     plan_start_date, plan_end_date, plan_type, plan_last_upd_user, plan_last_upd_date){
+        this.plan_code = plan_code;
+        this.plan_name = plan_name;
+        this.plan_desc = plan_desc;
+        this.plan_class = plan_class;
+        this.plan_initial_dis = plan_initial_dis;
+        this.plan_start_date = plan_start_date;
+        this.plan_end_date = plan_end_date;
+        this.plan_type = plan_type;
+        this.plan_last_upd_user = plan_last_upd_user;
+        this.plan_last_upd_date = plan_last_upd_date;
+    }
+}
--- src/app/plan/plan-list/plan-list.component.ts
+++ src/app/plan/plan-list/plan-list.component.ts
@@ -11,6 +11,8 @@ import { DataFilterPipe }   from '../../data-filter.pipe';
 import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { Router } from '@angular/router';
 
+import { PlanMst } from '../../common/models/plan-mst';
+
 @Component({
   selector: 'app-plan-list',
   templateUrl: './plan-list.component.html',
@@ -21,7 +23,7 @@ export class PlanListComponent implements OnInit {
 
   @ViewChild('modal')
   modal: ModalComponent;
-  items: string[] = ['item1', 'item2', 'item3'];
+  
   selected: string;
   output: string;
 
@@ -34,7 +36,15 @@ export class PlanListComponent implements OnInit {
   backdrop: string | boolean = true;
   css: boolean = false;
 
-  constructor(private http: Http, private router: Router) {}
+  planDetail : Array<PlanMst>;
+  titleDetail : string;
+
+  @ViewChild('modalSave')
+  modal1: ModalComponent;
+
+  constructor(private http: Http, private router: Router) {
+    this.planDetail = [];
+  }
 
   public data;
   public filterQuery = "";
@@ -75,13 +85,30 @@ export class PlanListComponent implements OnInit {
       this.router.navigateByUrl('/hello');
   }
 
-  public open(event, plan_code) {
+  public openDetail(event, plan_code) {
     event.preventDefault();
-    this.http.get(params.url + 'api/plan/show')
-          .subscribe((data)=> {
-              console.log('212121');
-          });
-    this.modal.open();
+    this.http.get(params.url + 'api/plan/detail/' + plan_code)
+          .subscribe(
+            response => {
+                  //khoi tao data
+                    let dataResponse = response.json().data;
+                    if (response.json().result === 1) {
+                      let plan = new PlanMst(dataResponse.plan_code, dataResponse.plan_name, dataResponse.plan_desc,
+                       dataResponse.plan_class, dataResponse.plan_initial_dis, dataResponse.plan_start_date, dataResponse.plan_end_date,
+                       dataResponse.plan_type, dataResponse.plan_last_upd_user, dataResponse.plan_last_upd_date);
+
+                      this.planDetail.push(plan);
+                      this.titleDetail = 'プラン詳細';
+                      this.modal.open();  
+                    } else {
+                      alert(response.json().message);  
+                    }
+                },
+                error => {
+                    alert(error.text());
+                    console.log(error.text());
+                }
+          );
   }
 
 }
